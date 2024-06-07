42547fdef81478453ff7eeb33e4f9f23635cc9fc
Patrick Lisby Córdoba <lisby2103@gmail.com>
2024-06-06 23:28:57 -0600

    Register incidents with img

--- src/app/registrar-incidencias/registrar-incidencias.component.ts
+++ src/app/registrar-incidencias/registrar-incidencias.component.ts
@@ -11,6 +11,7 @@ import { Router } from '@angular/router';
 })
 export class RegistrarIncidenciasComponent implements OnInit {
   incidenciaForm: FormGroup;
+  selectedFile: File | null = null; // Inicializar selectedFile con null
 
   constructor(
     private fb: FormBuilder, 
@@ -22,37 +23,54 @@ export class RegistrarIncidenciasComponent implements OnInit {
       ct_titulo_incidencia: ['', Validators.required],
       ct_descripcion_incidencia: ['', Validators.required],
       ct_lugar: ['', Validators.required],
-      cn_id_usuario_registro: ['1'] // Considera usar un valor dinámico si es necesario
+      cn_id_usuario_registro: [''], // Considera usar un valor dinámico si es necesario
+      image: [null, Validators.required]
     });
   }
 
   ngOnInit() {}
 
+  onFileSelected(event: any) { // Tipar explícitamente el parámetro event
+    this.selectedFile = event.target.files[0];
+    this.incidenciaForm.patchValue({ image: this.selectedFile });
+  }
+
   async onSubmit() {
     if (this.incidenciaForm.valid) {
       const { ct_titulo_incidencia, ct_descripcion_incidencia, ct_lugar, cn_id_usuario_registro } = this.incidenciaForm.value;
-      try {
-        await this.incidenciasService.registrar_incidencia(ct_titulo_incidencia, ct_descripcion_incidencia, ct_lugar, cn_id_usuario_registro);
-        console.log('Incidencia registrada con éxito');
-        const successToast = await this.toastController.create({
-          message: 'Incidencia registrada con éxito',
+      if (this.selectedFile) { // Verificar si selectedFile no es null
+        try {
+          await this.incidenciasService.registrar_incidencia(ct_titulo_incidencia, ct_descripcion_incidencia, ct_lugar, cn_id_usuario_registro, this.selectedFile);
+          console.log('Incidencia registrada con éxito');
+          const successToast = await this.toastController.create({
+            message: 'Incidencia registrada con éxito',
+            duration: 1500,
+            position: 'top',
+            color: 'success'
+          });
+          await successToast.present();
+          
+          // Navegar a la ruta 'ver_incidencias' después del éxito
+          this.router.navigate(['/ver_incidencias']);
+        } catch (error) {
+          console.error('Error al registrar la incidencia:', error);
+          const errorToast = await this.toastController.create({
+            message: 'Error al registrar la incidencia',
+            duration: 1500,
+            position: 'top',
+            color: 'danger'
+          });
+          await errorToast.present();
+        }
+      } else {
+        console.log('No se ha seleccionado ningún archivo');
+        const noFileToast = await this.toastController.create({
+          message: 'Por favor, seleccione un archivo',
           duration: 1500,
           position: 'top',
-          color: 'success'
+          color: 'warning'
         });
-        await successToast.present();
-        
-        // Navegar a la ruta 'ver_incidencias' después del éxito
-        this.router.navigate(['/ver_incidencias']);
-      } catch (error) {
-        console.error('Error al registrar la incidencia:', error);
-        const errorToast = await this.toastController.create({
-          message: 'Error al registrar la incidencia',
-          duration: 1500,
-          position: 'top',
-          color: 'danger'
-        });
-        await errorToast.present();
+        await noFileToast.present();
       }
     } else {
       console.log('Formulario no válido');
@@ -64,11 +82,9 @@ export class RegistrarIncidenciasComponent implements OnInit {
       });
       await invalidFormToast.present();
     }
-  }//End of submit
+  }
 
   navigateToIncidencias() {
     this.router.navigate(['/ver_incidencias']);
   }
-
-
 }
--- src/app/services/incidencias.service.ts
+++ src/app/services/incidencias.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AlertController } from '@ionic/angular';
 
 @Injectable({
@@ -14,14 +14,11 @@ export class IncidenciasService {
   async mostrar_incidencias(): Promise<any> {
     try {
       const token = localStorage.getItem(this.tokenKey);
-      //console.log(token);
       const headers = new HttpHeaders({
         'Authorization': `Bearer ${token}`
       });
-      const response = await this.http.get(`${this.apiURL}mostrar_incidentes`).toPromise();
-      //console.log('Mostrar incidencias', response);
+      const response = await this.http.get(`${this.apiURL}mostrar_incidentes`, { headers }).toPromise();
       return response;
-      
     } catch (error) {
       console.error('Login error:', error);
     }
@@ -30,61 +27,50 @@ export class IncidenciasService {
   async mostrar_incidencias_por_id(ct_id_incidencia: string): Promise<any> {
     try {
       const token = localStorage.getItem(this.tokenKey);
-      //console.log(token);
       const headers = new HttpHeaders({
         'Authorization': `Bearer ${token}`
       });
-     // console.log(ct_id_incidencia + " Esto es lo que tiene de id");
-      const response = await this.http.get(`${this.apiURL}mostrar_incidentes_por_id/${ct_id_incidencia}`).toPromise();
-      //console.log('Mostrar incidencias', response);
+      const response = await this.http.get(`${this.apiURL}mostrar_incidentes_por_id/${ct_id_incidencia}`, { headers }).toPromise();
       return response;
     } catch (error) {
       console.error('Error en mostrar_incidencias_por_id:', error);
       throw error;
     }
-  }//End of mostrar_incidencias_por_id
+  }
 
   async mostrar_incidencias_por_usuario(ct_id_incidencia: string): Promise<any> {
     try {
       const token = localStorage.getItem(this.tokenKey);
-      //console.log(token);
       const headers = new HttpHeaders({
         'Authorization': `Bearer ${token}`
       });
-      //console.log(ct_id_incidencia + " Esto es lo que tiene de id");
-      
-      const response = await this.http.get(`${this.apiURL}mostrar_incidencias_por_usuario/${ct_id_incidencia}`).toPromise();
-    //  console.log('Mostrar incidencias', response);
+      const response = await this.http.get(`${this.apiURL}mostrar_incidencias_por_usuario/${ct_id_incidencia}`, { headers }).toPromise();
       return response;
     } catch (error) {
       console.error('Error en mostrar_incidencias_por_id:', error);
       throw error;
     }
-  }//End of mostrar_incidencias_por_id
+  }
 
-  async registrar_incidencia(ct_titulo_incidencia: string, ct_descripcion_incidencia: string, ct_lugar: string, cn_id_usuario_registro: number): Promise<any> {
+  async registrar_incidencia(ct_titulo_incidencia: string, ct_descripcion_incidencia: string, ct_lugar: string, cn_id_usuario_registro: number, image: File): Promise<any> {
     try {
       const token = localStorage.getItem(this.tokenKey);
-      //console.log(token);
       const headers = new HttpHeaders({
         'Authorization': `Bearer ${token}`
       });
 
-      const requestBody = {
-        ct_titulo_incidencia,
-        ct_descripcion_incidencia,
-        ct_lugar,
-        cn_id_usuario_registro,
-      };
+      const formData = new FormData();
+      formData.append('ct_titulo_incidencia', ct_titulo_incidencia);
+      formData.append('ct_descripcion_incidencia', ct_descripcion_incidencia);
+      formData.append('ct_lugar', ct_lugar);
+      formData.append('cn_id_usuario_registro', cn_id_usuario_registro.toString());
+      formData.append('image', image);
 
-      const response = await this.http.post(`${this.apiURL}registrar_incidencia`, requestBody).toPromise();
-      //console.log('Incidencia registrada:', response);
+      const response = await this.http.post(`${this.apiURL}registrar_incidencia`, formData, { headers }).toPromise();
       return response;
     } catch (error) {
       console.error('Error al registrar la incidencia:', error);
       throw error;
     }
   }
-
-  
 }
