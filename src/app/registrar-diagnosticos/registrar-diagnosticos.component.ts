1ffffff4a4fc26ce896bf3d8f63f5bd705fbef9d
Patrick Lisby Córdoba <lisby2103@gmail.com>
2024-06-07 00:00:42 -0600

    Fix diagnosticos img

--- src/app/registrar-diagnosticos/registrar-diagnosticos.component.ts
+++ src/app/registrar-diagnosticos/registrar-diagnosticos.component.ts
@@ -4,7 +4,6 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DiagnosticosService } from '../services/diagnosticos.service';
 import { ToastController } from '@ionic/angular';
 
-
 @Component({
   selector: 'app-registrar-diagnosticos',
   templateUrl: './registrar-diagnosticos.component.html',
@@ -13,6 +12,7 @@ import { ToastController } from '@ionic/angular';
 export class RegistrarDiagnosticosComponent implements OnInit {
   diagnosticoForm: FormGroup;
   ct_id_incidencia: string;
+  selectedFile: File | null = null;
 
   constructor(
     private fb: FormBuilder, 
@@ -27,52 +27,58 @@ export class RegistrarDiagnosticosComponent implements OnInit {
       cn_tiempo_estimado_reparacion: ['', Validators.required],
       ct_observaciones: ['', Validators.required],
       ct_id_incidencia: ['', Validators.required],
-      cn_id_usuario: ['1'] // Considera usar un valor dinámico si es necesario
+      cn_id_usuario: ['1', Validators.required],
+      image: [null, Validators.required]
     });
   }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.ct_id_incidencia = params.get('ct_id_incidencia') ?? '';
-      // Asigna el valor de ct_id_incidencia al control en el FormGroup
       this.diagnosticoForm.patchValue({ct_id_incidencia: this.ct_id_incidencia});
     });
+  }
+
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+    this.diagnosticoForm.patchValue({ image: this.selectedFile });
   }
 
   async onSubmit() {
     if (this.diagnosticoForm.valid) {
       const { ct_diagnostico, cn_tiempo_estimado_reparacion, ct_observaciones, ct_id_incidencia, cn_id_usuario } = this.diagnosticoForm.value;
-      //console.log("Diagnostico: "+ct_diagnostico+ "\n"+"Tiempo: "+cn_tiempo_estimado_reparacion+ "\n" +"Observacion: "+ct_observaciones+ "\n"+"Id Inicidencia: "+ct_id_incidencia+ "\n"+"id usuario: "+cn_id_usuario+ "\n");
-      try {
-        // Llama al servicio para registrar el diagnóstico
-       const result =  await this.diagnosticosService.registrar_diagnosticos(ct_diagnostico, cn_tiempo_estimado_reparacion, ct_observaciones, ct_id_incidencia, cn_id_usuario);
-        console.log(result);
-        
-        console.log('Diagnóstico registrado con éxito');
-        
-        // Muestra un mensaje de éxito
-        const successToast = await this.toastController.create({
-          message: 'Diagnóstico registrado con éxito',
+      if (this.selectedFile) {
+        try {
+          await this.diagnosticosService.registrar_diagnosticos(ct_diagnostico, cn_tiempo_estimado_reparacion, ct_observaciones, ct_id_incidencia, cn_id_usuario, this.selectedFile);
+          const successToast = await this.toastController.create({
+            message: 'Diagnóstico registrado con éxito',
+            duration: 1500,
+            position: 'top',
+            color: 'success'
+          });
+          await successToast.present();
+          this.navigateToIncidencias(ct_id_incidencia);
+        } catch (error: any) {
+          console.error('Error al registrar el diagnóstico:', error);
+          const errorToast = await this.toastController.create({
+            message: 'Error al registrar el diagnóstico',
+            duration: 1500,
+            position: 'top',
+            color: 'danger'
+          });
+          await errorToast.present();
+        }
+      } else {
+        console.log('No se ha seleccionado ningún archivo');
+        const noFileToast = await this.toastController.create({
+          message: 'Por favor, seleccione un archivo',
           duration: 1500,
           position: 'top',
-          color: 'success'
+          color: 'warning'
         });
-        await successToast.present();
-        this.navigateToIncidencias(ct_id_incidencia);
-      } catch (error: any) { // Añade ": any" para especificar que error es de tipo any
-        console.error('Error al registrar el diagnóstico:', error);
-        
-        // Muestra un mensaje de error
-        const errorToast = await this.toastController.create({
-          message: 'Error al registrar el diagnóstico',
-          duration: 1500,
-          position: 'top',
-          color: 'danger'
-        });
-        await errorToast.present();
+        await noFileToast.present();
       }
     } else {
-      // Si el formulario no es válido, muestra un mensaje
       console.log('Formulario no válido');
       const invalidFormToast = await this.toastController.create({
         message: 'Por favor, complete todos los campos requeridos',
@@ -83,8 +89,6 @@ export class RegistrarDiagnosticosComponent implements OnInit {
       await invalidFormToast.present();
     }
   }
-  
-  
 
   navigateToIncidencias(ct_id_incidencia: string) {
     this.router.navigate(['/ver_incidencias_completa', ct_id_incidencia]);
--- src/app/services/diagnosticos.service.ts
+++ src/app/services/diagnosticos.service.ts
@@ -1,96 +1,78 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AlertController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DiagnosticosService {
-
-  
   private apiURL = 'http://127.0.0.1:3000/';
   private tokenKey = 'authToken';
-  constructor(private http: HttpClient, private alertController: AlertController) { }
 
+  constructor(private http: HttpClient, private alertController: AlertController) { }
 
   async mostrar_diagnosticos_por_id(ct_id_incidencia: string): Promise<any> {
     try {
       const token = localStorage.getItem(this.tokenKey);
-      //console.log(token);
       const headers = new HttpHeaders({
         'Authorization': `Bearer ${token}`
       });
-      console.log(ct_id_incidencia + " Esto es lo que tiene de id");
-      
-      const response = await this.http.get(`${this.apiURL}mostrar_diagnosticos_por_id/${ct_id_incidencia}`).toPromise();
-      console.log('Mostrar diagnosticos', response);
+      const response = await this.http.get(`${this.apiURL}mostrar_diagnosticos_por_id/${ct_id_incidencia}`, { headers }).toPromise();
       return response;
     } catch (error) {
       console.error('Error en mostrar_diagnosticos_por_id:', error);
       throw error;
     }
-  }//End of mostrar_incidencias_por_id
+  }
 
   async mostrar_diagnosticos_por_usuario(ct_id_incidencia: string): Promise<any> {
     try {
       const token = localStorage.getItem(this.tokenKey);
-      //console.log(token);
       const headers = new HttpHeaders({
         'Authorization': `Bearer ${token}`
       });
-      console.log(ct_id_incidencia + " Esto es lo que tiene de id");
-      
-      const response = await this.http.get(`${this.apiURL}mostrar_diagnosticos_por_usuario/${ct_id_incidencia}`).toPromise();
-      console.log('Mostrar diagnosticos', response);
+      const response = await this.http.get(`${this.apiURL}mostrar_diagnosticos_por_usuario/${ct_id_incidencia}`, { headers }).toPromise();
       return response;
     } catch (error) {
-      console.error('Error en mostrar_diagnosticos_por_id:', error);
+      console.error('Error en mostrar_diagnosticos_por_usuario:', error);
       throw error;
     }
-  }//End of mostrar_incidencias_por_id
+  }
 
-  async registrar_diagnosticos(ct_diagnostico:string, cn_tiempo_estimado_reparacion:string, ct_observaciones:string, ct_id_incidencia:string, cn_id_usuario: number): Promise<any> {
+  async registrar_diagnosticos(ct_diagnostico: string, cn_tiempo_estimado_reparacion: string, ct_observaciones: string, ct_id_incidencia: string, cn_id_usuario: number, image: File): Promise<any> {
     try {
       const token = localStorage.getItem(this.tokenKey);
-      //console.log(token);
       const headers = new HttpHeaders({
         'Authorization': `Bearer ${token}`
       });
-      const requestBody = {
-        ct_diagnostico,
-        cn_tiempo_estimado_reparacion,
-        ct_observaciones,
-        ct_id_incidencia,
-        cn_id_usuario,
-      };
-      //console.log(requestBody);
 
-      const response = await this.http.post(`${this.apiURL}registrar_diagnosticos`, requestBody).toPromise();
-      console.log('Diagnostico registrada:', response);
+      const formData = new FormData();
+      formData.append('ct_diagnostico', ct_diagnostico);
+      formData.append('cn_tiempo_estimado_reparacion', cn_tiempo_estimado_reparacion);
+      formData.append('ct_observaciones', ct_observaciones);
+      formData.append('ct_id_incidencia', ct_id_incidencia);
+      formData.append('cn_id_usuario', cn_id_usuario.toString());
+      formData.append('image', image);
+
+      const response = await this.http.post(`${this.apiURL}registrar_diagnosticos`, formData, { headers }).toPromise();
       return response;
     } catch (error) {
       console.error('Hubo un error en registro de diagnosticos', error);
       throw error;
     }
-  }//End of registrar_diagnosticos
+  }
 
   async mostrar_diagnosticos_por_id_incidencia(ct_id_incidencia: string): Promise<any> {
     try {
       const token = localStorage.getItem(this.tokenKey);
-      //console.log(token);
       const headers = new HttpHeaders({
         'Authorization': `Bearer ${token}`
       });
-      console.log(ct_id_incidencia + " Esto es lo que tiene de id");
-      
-      const response = await this.http.get(`${this.apiURL}mostrar_diagnosticos_id_incidencia/${ct_id_incidencia}`).toPromise();
-      console.log('Mostrar diagnosticos por id incidentes', response);
+      const response = await this.http.get(`${this.apiURL}mostrar_diagnosticos_id_incidencia/${ct_id_incidencia}`, { headers }).toPromise();
       return response;
     } catch (error) {
       console.error('Error en mostrar_diagnosticos_por_id:', error);
       throw error;
     }
-  }//End of mostrar_diagnosticos_por_id_incidencia
-
-  
+  }
 }
